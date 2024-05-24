d5585261a149c4cf63446b5c67c3ce08c187c2c3
Julia <juliatonetx@gmail.com>
2024-05-25 01:41:10 +0800

    Arrumando o código

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
+import "dotenv/config";
+import "reflect-metadata";
+import "express-async-errors";
 import express, { json } from "express";
 import helmet from "helmet";
+import cors from "cors";
+import { carRouter } from "./routes/car.routes";
+import { HandleErrors } from "./errors/handleErrors";
+
 export const app = express();
+app.use(cors());
 app.use(json());
 app.use(helmet());
+app.use("/cars", carRouter);
+app.use(HandleErrors.execute);
