5bf643489a5f5011707510e573753562f624adeb
Julia <juliatonetx@gmail.com>
2024-05-26 22:19:37 +0800

    instalando supertest e criando mocks

---
+++ src/__tests__/__mocks__/car.mock.ts
@@ -0,0 +1,35 @@
+export const carCreateMock = {
+  name: "Car name",
+  description: "Car description",
+  brand: "Card brand",
+  year: 2024,
+  km: 10000,
+};
+
+export const carMock = {
+  id: "fe111d24-1b79-44df-931b-4c9fd5859014",
+  name: "Car name",
+  description: "Car description",
+  brand: "Card brand",
+  year: 2024,
+  km: 10000,
+};
+
+export const carFindManyMock = [
+  {
+    id: "fe111d24-1b79-44df-931b-4c9fd5859014",
+    name: "Car name",
+    description: "Car description",
+    brand: "Card brand",
+    year: 2024,
+    km: 10000,
+  },
+  {
+    id: "0bb1764a-3ce1-4dcc-ae96-f502b78a90bc",
+    name: "Car name",
+    description: "Car description",
+    brand: "Card brand",
+    year: 2024,
+    km: 10000,
+  },
+];
