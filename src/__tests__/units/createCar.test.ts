e28282f336b34c1d82f75a177f56d3af258b6e8b
Julia <juliatonetx@gmail.com>
2024-05-28 17:34:32 +0800

    Ajustes para integração e criação do car create test:unit

---
+++ src/__tests__/units/createCar.test.ts
@@ -0,0 +1,12 @@
+import { CarServices } from "../../services/car.services";
+import { prismaMock } from "../__mocks__/prisma";
+import { carMock, carCreateMock } from "../__mocks__/car.mock";
+
+describe("Unit test: create car", () => {
+  test("create car should work correctly", async () => {
+    const carService = new CarServices();
+    prismaMock.car.create.mockResolvedValue(carMock);
+    const data = await carService.create(carCreateMock);
+    expect(data).toStrictEqual(carMock);
+  });
+});
